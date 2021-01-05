ba6ea27ffb71031ca9588ead594333bce2092ec9
coder-richa <coder.richa@gmail.com>
2021-01-05 18:23:17 +0530

    Declaring variables and constants.

---
+++ basics/02-declaring-constants.js
@@ -0,0 +1,9 @@
+/* The Below directive bound to follow JS norms */
+"use strict";
+
+/***********************************************
+ * Declaring constants using let keyword
+ */
+
+const PI = 3.14;
+console.log("PI: ", PI);

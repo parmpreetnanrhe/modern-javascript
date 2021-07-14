031d1dc05a4af850b11d51d9327e02f6fb0d86ba
coder-richa <coder.richa@gmail.com>
2021-07-14 21:40:16 +0530

    Using bracket operator to toggle model class

--- dom-manipulator-and-events/03-create-modal/script.js
+++ dom-manipulator-and-events/03-create-modal/script.js
@@ -4,18 +4,25 @@ let modalButtons = document.querySelectorAll(".show-modal");
 let modal = document.querySelector(".modal");
 let overlay = document.querySelector(".overlay");
 let closeButton = document.querySelector(".close-modal");
+const updateProperty = (elem, operation, className) => {
+  elem[operation](className);
+};
+
 const openModal = () => {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  updateProperty(modal.classList, "remove", "hidden");
+  updateProperty(overlay.classList, "remove", "hidden");
 };
 const closeModal = () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  // modal.classList.add("hidden");
+  // overlay.classList.add("hidden");
+  updateProperty(modal.classList, "add", "hidden");
+  updateProperty(overlay.classList, "add", "hidden");
 };
 
 // Register event listener
 modalButtons.forEach((btn) => btn.addEventListener("click", openModal));
 closeButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 document.addEventListener("keydown", (event) => {
   if (event.key == "Escape") closeModal();
 });
